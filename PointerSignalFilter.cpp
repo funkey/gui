f66f0fc374b33ba6f225587ab2f2ac3e2514bfc2
Jan Funke <funke@ini.ch>
2013-09-06 11:36:24 +0200

    made PointerSignalFilter handle multi inputs properly

--- PointerSignalFilter.cpp
+++ PointerSignalFilter.cpp
@@ -2,107 +2,100 @@
 
 namespace gui {
 
+#define POINTER_SIGNAL_FILTER_REGISTER_FORWARD_CALLBACK(s) \
+	output.registerForwardCallback(boost::function<void(s&)>(boost::bind(&PointerSignalFilter::on##s, this, _1)), processNode, signals::Transparent);
+
 void
 PointerSignalFilter::filterBackward(pipeline::OutputBase& output, pipeline::InputBase& input, pipeline::ProcessNode* processNode) {
 
-	output.registerForwardCallback(boost::function<void(FingerUp&)>(boost::bind(&PointerSignalFilter::onFingerUp,   this, _1)), processNode, signals::Transparent);
-	output.registerForwardCallback(boost::function<void(FingerDown&)>(boost::bind(&PointerSignalFilter::onFingerDown, this, _1)), processNode, signals::Transparent);
-	output.registerForwardCallback(boost::function<void(FingerMove&)>(boost::bind(&PointerSignalFilter::onFingerMove, this, _1)), processNode, signals::Transparent);
-	output.registerForwardCallback(boost::function<void(PenUp&)>(boost::bind(&PointerSignalFilter::onPenUp,   this, _1)), processNode, signals::Transparent);
-	output.registerForwardCallback(boost::function<void(PenDown&)>(boost::bind(&PointerSignalFilter::onPenDown, this, _1)), processNode, signals::Transparent);
-	output.registerForwardCallback(boost::function<void(PenMove&)>(boost::bind(&PointerSignalFilter::onPenMove, this, _1)), processNode, signals::Transparent);
-	output.registerForwardCallback(boost::function<void(PenIn&)>(boost::bind(&PointerSignalFilter::onPenIn, this, _1)), processNode, signals::Transparent);
-	output.registerForwardCallback(boost::function<void(PenOut&)>(boost::bind(&PointerSignalFilter::onPenOut, this, _1)), processNode, signals::Transparent);
-	output.registerForwardCallback(boost::function<void(PenAway&)>(boost::bind(&PointerSignalFilter::onPenAway, this, _1)), processNode, signals::Transparent);
-	output.registerForwardCallback(boost::function<void(MouseUp&)>(boost::bind(&PointerSignalFilter::onMouseUp,   this, _1)), processNode, signals::Transparent);
-	output.registerForwardCallback(boost::function<void(MouseDown&)>(boost::bind(&PointerSignalFilter::onMouseDown, this, _1)), processNode, signals::Transparent);
-	output.registerForwardCallback(boost::function<void(MouseMove&)>(boost::bind(&PointerSignalFilter::onMouseMove, this, _1)), processNode, signals::Transparent);
+	POINTER_SIGNAL_FILTER_REGISTER_FORWARD_CALLBACK(FingerUp)
+	POINTER_SIGNAL_FILTER_REGISTER_FORWARD_CALLBACK(FingerDown)
+	POINTER_SIGNAL_FILTER_REGISTER_FORWARD_CALLBACK(FingerMove)
+	POINTER_SIGNAL_FILTER_REGISTER_FORWARD_CALLBACK(PenUp)
+	POINTER_SIGNAL_FILTER_REGISTER_FORWARD_CALLBACK(PenDown)
+	POINTER_SIGNAL_FILTER_REGISTER_FORWARD_CALLBACK(PenMove)
+	POINTER_SIGNAL_FILTER_REGISTER_FORWARD_CALLBACK(MouseUp)
+	POINTER_SIGNAL_FILTER_REGISTER_FORWARD_CALLBACK(MouseDown)
+	POINTER_SIGNAL_FILTER_REGISTER_FORWARD_CALLBACK(MouseMove)
 
-	input.registerBackwardSlot(_fingerUp);
-	input.registerBackwardSlot(_fingerDown);
-	input.registerBackwardSlot(_fingerMove);
-	input.registerBackwardSlot(_penUp);
-	input.registerBackwardSlot(_penDown);
-	input.registerBackwardSlot(_penMove);
-	input.registerBackwardSlot(_penIn);
-	input.registerBackwardSlot(_penOut);
-	input.registerBackwardSlot(_penAway);
-	input.registerBackwardSlot(_mouseUp);
-	input.registerBackwardSlot(_mouseDown);
-	input.registerBackwardSlot(_mouseMove);
+	input.registerBackwardSlot(_slotFingerUp);
+	input.registerBackwardSlot(_slotFingerDown);
+	input.registerBackwardSlot(_slotFingerMove);
+	input.registerBackwardSlot(_slotPenUp);
+	input.registerBackwardSlot(_slotPenDown);
+	input.registerBackwardSlot(_slotPenMove);
+	input.registerBackwardSlot(_slotPenIn);
+	input.registerBackwardSlot(_slotPenOut);
+	input.registerBackwardSlot(_slotPenAway);
+	input.registerBackwardSlot(_slotMouseUp);
+	input.registerBackwardSlot(_slotMouseDown);
+	input.registerBackwardSlot(_slotMouseMove);
 }
+
+#define POINTER_SIGNAL_FILTER_REGISTER_FORWARD_CALLBACK_MULTI(s) \
+	output.registerForwardCallback(boost::function<void(s&)>(boost::bind(&PointerSignalFilter::onMulti##s, this, _1)), processNode, signals::Transparent);
 
 void
-PointerSignalFilter::onFingerUp(FingerUp& signal) {
+PointerSignalFilter::filterBackward(pipeline::OutputBase& output, pipeline::MultiInput& inputs, pipeline::ProcessNode* processNode) {
 
-	filterSignal(signal, _fingerUp);
+	POINTER_SIGNAL_FILTER_REGISTER_FORWARD_CALLBACK_MULTI(FingerUp)
+	POINTER_SIGNAL_FILTER_REGISTER_FORWARD_CALLBACK_MULTI(FingerDown)
+	POINTER_SIGNAL_FILTER_REGISTER_FORWARD_CALLBACK_MULTI(FingerMove)
+	POINTER_SIGNAL_FILTER_REGISTER_FORWARD_CALLBACK_MULTI(PenUp)
+	POINTER_SIGNAL_FILTER_REGISTER_FORWARD_CALLBACK_MULTI(PenDown)
+	POINTER_SIGNAL_FILTER_REGISTER_FORWARD_CALLBACK_MULTI(PenMove)
+	POINTER_SIGNAL_FILTER_REGISTER_FORWARD_CALLBACK_MULTI(MouseUp)
+	POINTER_SIGNAL_FILTER_REGISTER_FORWARD_CALLBACK_MULTI(MouseDown)
+	POINTER_SIGNAL_FILTER_REGISTER_FORWARD_CALLBACK_MULTI(MouseMove)
+
+	inputs.registerBackwardSlots(_slotsFingerUp);
+	inputs.registerBackwardSlots(_slotsFingerDown);
+	inputs.registerBackwardSlots(_slotsFingerMove);
+	inputs.registerBackwardSlots(_slotsPenUp);
+	inputs.registerBackwardSlots(_slotsPenDown);
+	inputs.registerBackwardSlots(_slotsPenMove);
+	inputs.registerBackwardSlots(_slotsPenIn);
+	inputs.registerBackwardSlots(_slotsPenOut);
+	inputs.registerBackwardSlots(_slotsPenAway);
+	inputs.registerBackwardSlots(_slotsMouseUp);
+	inputs.registerBackwardSlots(_slotsMouseDown);
+	inputs.registerBackwardSlots(_slotsMouseMove);
 }
 
-void
-PointerSignalFilter::onFingerDown(FingerDown& signal) {
+#define POINTER_SIGNAL_FILTER_ON(s) \
+	void \
+	PointerSignalFilter::on##s(s& signal) { \
+	\
+		filterSignal(signal, _slot##s); \
+	}
 
-	filterSignal(signal, _fingerDown);
-}
+POINTER_SIGNAL_FILTER_ON(FingerUp)
+POINTER_SIGNAL_FILTER_ON(FingerDown)
+POINTER_SIGNAL_FILTER_ON(FingerMove)
+POINTER_SIGNAL_FILTER_ON(PenUp)
+POINTER_SIGNAL_FILTER_ON(PenDown)
+POINTER_SIGNAL_FILTER_ON(PenMove)
+POINTER_SIGNAL_FILTER_ON(MouseUp)
+POINTER_SIGNAL_FILTER_ON(MouseDown)
+POINTER_SIGNAL_FILTER_ON(MouseMove)
 
-void
-PointerSignalFilter::onFingerMove(FingerMove& signal) {
+#define POINTER_SIGNAL_FILTER_ON_MULTI(s) \
+	void \
+	PointerSignalFilter::onMulti##s(s& signal) { \
+	\
+		for (unsigned int i = 0; i < _slots##s.size(); i++) \
+			filterSignal(signal, i, _slots##s); \
+	}
 
-	filterSignal(signal, _fingerMove);
-}
-
-void
-PointerSignalFilter::onPenUp(PenUp& signal) {
-
-	filterSignal(signal, _penUp);
-}
-
-void
-PointerSignalFilter::onPenDown(PenDown& signal) {
-
-	filterSignal(signal, _penDown);
-}
-
-void
-PointerSignalFilter::onPenMove(PenMove& signal) {
-
-	filterSignal(signal, _penMove);
-}
-
-void
-PointerSignalFilter::onPenIn(PenIn& signal) {
-
-	filterSignal(signal, _penIn);
-}
-
-void
-PointerSignalFilter::onPenOut(PenOut& signal) {
-
-	filterSignal(signal, _penOut);
-}
-
-void
-PointerSignalFilter::onPenAway(PenAway& signal) {
-
-	filterSignal(signal, _penAway);
-}
-
-void
-PointerSignalFilter::onMouseUp(MouseUp& signal) {
-
-	filterSignal(signal, _mouseUp);
-}
-
-void
-PointerSignalFilter::onMouseDown(MouseDown& signal) {
-
-	filterSignal(signal, _mouseDown);
-}
-
-void
-PointerSignalFilter::onMouseMove(MouseMove& signal) {
-
-	filterSignal(signal, _mouseMove);
-}
+POINTER_SIGNAL_FILTER_ON_MULTI(FingerUp)
+POINTER_SIGNAL_FILTER_ON_MULTI(FingerDown)
+POINTER_SIGNAL_FILTER_ON_MULTI(FingerMove)
+POINTER_SIGNAL_FILTER_ON_MULTI(PenUp)
+POINTER_SIGNAL_FILTER_ON_MULTI(PenDown)
+POINTER_SIGNAL_FILTER_ON_MULTI(PenMove)
+POINTER_SIGNAL_FILTER_ON_MULTI(MouseUp)
+POINTER_SIGNAL_FILTER_ON_MULTI(MouseDown)
+POINTER_SIGNAL_FILTER_ON_MULTI(MouseMove)
 
 template <typename SignalType>
 void
@@ -113,6 +106,17 @@ PointerSignalFilter::filterSignal(SignalType& signal, signals::Slot<SignalType>&
 	filter(signal);
 
 	slot(signal);
+}
+
+template <typename SignalType>
+void
+PointerSignalFilter::filterSignal(SignalType& signal, unsigned int i, signals::Slots<SignalType>& slots) {
+
+	if (signal.processed)
+		return;
+	filter(signal, i);
+
+	slots[i](signal);
 }
 
 } // namespace gui
--- PointerSignalFilter.h
+++ PointerSignalFilter.h
@@ -15,6 +15,8 @@ public:
 
 	void filterBackward(pipeline::OutputBase& output, pipeline::InputBase& input, pipeline::ProcessNode* processNode);
 
+	void filterBackward(pipeline::OutputBase& output, pipeline::MultiInput& inputs, pipeline::ProcessNode* processNode);
+
 protected:
 
 	/**
@@ -26,47 +28,71 @@ protected:
 	 */
 	virtual bool filter(PointerSignal& signal) = 0;
 
+	/**
+	 * Filter method for one-to-many filters (from one output to several 
+	 * inputs). Will be called with the appropriate input number.
+	 */
+	virtual bool filter(PointerSignal& signal, unsigned int input) { return true; }
+
 private:
 
 	void onFingerUp(FingerUp& signal);
-
 	void onFingerDown(FingerDown& signal);
-
 	void onFingerMove(FingerMove& signal);
-
 	void onPenUp(PenUp& signal);
-
 	void onPenDown(PenDown& signal);
-
 	void onPenMove(PenMove& signal);
-
 	void onPenIn(PenIn& signal);
-
 	void onPenOut(PenOut& signal);
-
 	void onPenAway(PenAway& signal);
-
 	void onMouseUp(MouseUp& signal);
-
 	void onMouseDown(MouseDown& signal);
-
 	void onMouseMove(MouseMove& signal);
+
+	void onMultiFingerUp(FingerUp& signal);
+	void onMultiFingerDown(FingerDown& signal);
+	void onMultiFingerMove(FingerMove& signal);
+	void onMultiPenUp(PenUp& signal);
+	void onMultiPenDown(PenDown& signal);
+	void onMultiPenMove(PenMove& signal);
+	void onMultiPenIn(PenIn& signal);
+	void onMultiPenOut(PenOut& signal);
+	void onMultiPenAway(PenAway& signal);
+	void onMultiMouseUp(MouseUp& signal);
+	void onMultiMouseDown(MouseDown& signal);
+	void onMultiMouseMove(MouseMove& signal);
 
 	template <typename SignalType>
 	void filterSignal(SignalType& signal, signals::Slot<SignalType>& slot);
 
-	signals::Slot<FingerUp>   _fingerUp;
-	signals::Slot<FingerDown> _fingerDown;
-	signals::Slot<FingerMove> _fingerMove;
-	signals::Slot<PenUp>      _penUp;
-	signals::Slot<PenDown>    _penDown;
-	signals::Slot<PenMove>    _penMove;
-	signals::Slot<PenIn>      _penIn;
-	signals::Slot<PenOut>     _penOut;
-	signals::Slot<PenAway>    _penAway;
-	signals::Slot<MouseUp>    _mouseUp;
-	signals::Slot<MouseDown>  _mouseDown;
-	signals::Slot<MouseMove>  _mouseMove;
+	template <typename SignalType>
+	void filterSignal(SignalType& signal, unsigned int i, signals::Slots<SignalType>& slots);
+
+	signals::Slot<FingerUp>   _slotFingerUp;
+	signals::Slot<FingerDown> _slotFingerDown;
+	signals::Slot<FingerMove> _slotFingerMove;
+	signals::Slot<PenUp>      _slotPenUp;
+	signals::Slot<PenDown>    _slotPenDown;
+	signals::Slot<PenMove>    _slotPenMove;
+	signals::Slot<PenIn>      _slotPenIn;
+	signals::Slot<PenOut>     _slotPenOut;
+	signals::Slot<PenAway>    _slotPenAway;
+	signals::Slot<MouseUp>    _slotMouseUp;
+	signals::Slot<MouseDown>  _slotMouseDown;
+	signals::Slot<MouseMove>  _slotMouseMove;
+
+	signals::Slots<FingerUp>   _slotsFingerUp;
+	signals::Slots<FingerDown> _slotsFingerDown;
+	signals::Slots<FingerMove> _slotsFingerMove;
+	signals::Slots<PenUp>      _slotsPenUp;
+	signals::Slots<PenDown>    _slotsPenDown;
+	signals::Slots<PenMove>    _slotsPenMove;
+	signals::Slots<PenIn>      _slotsPenIn;
+	signals::Slots<PenOut>     _slotsPenOut;
+	signals::Slots<PenAway>    _slotsPenAway;
+	signals::Slots<MouseUp>    _slotsMouseUp;
+	signals::Slots<MouseDown>  _slotsMouseDown;
+	signals::Slots<MouseMove>  _slotsMouseMove;
 };
 
 };
